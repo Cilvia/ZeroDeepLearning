3689cb55b8786d10ed85e5edacc0d5bb90fb0419
cilvia <cilvia@cilvianoMacBook-Pro.local>
2017-07-19 22:12:09 +0900

    first commit

---
+++ neural_3lay.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def init_network():
+    network = {}
+    network['w1'] = np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
+    network['b1'] = np.array([0.1,0.2,0.3])
+    network['w2'] = np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+    network['b2'] = np.array([0.1,0.2])
+    network['w3'] = np.array([[0.1,0.3],[0.2,0.4]])
+    network['b3'] = np.array([0.1,0.2])
+
+    return network
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+def identity_function(x):
+    return softmax(x)
+
+def forword(network,x):
+    w1,w2,w3 = network['w1'],network['w2'],network['w3']
+    b1,b2,b3 = network['b1'],network['b2'],network['b3']
+
+    a1 = np.dot(x,w1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1,w2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2,w3) + b3
+    y = identity_function(a3)
+
+    return y
+
+network = init_network()
+x = np.array([1.0,0.5])
+y = forword(network,x)
+print(y)
+print(np.sum(y))
